febac94e1d4f2cee5eb5ff9f25e6302f6b489efb
nickmorris991 <nickmorris25@outlook.com>
2019-10-11 00:17:55 -0400

    updated server.py

--- client.py
+++ client.py
@@ -1,21 +1,25 @@
-import select, socket, sys, Queue
+import select, socket, sys;
 # Echo client program
-
-import socket
-import time
 count = 0
-HOST = 'localhost'    # The remote host
-PORT = 5001           # The same port as used by the server
+HOST = socket.gethostname()    # The remote host
+PORT = 5000                    # The same port as used by the server
+
+#establish socket and connection to the IP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-inputs = [s, sys.stdin]
-command_string = raw_input("enter string for me to keep echoing to server -> ")
-while 1:    
-	count = count+1    
-	msgToSend = command_string    
-	s.sendall(msgToSend)    
-	data = s.recv(1024)    
-	print 'Received', repr(data)    
-	time.sleep(5)
+
+message = s.recv(1024)
+print(message.decode("utf-8"))
+
+
+# inputs = [s, sys.stdin]
+# command_string = raw_input("enter string for me to keep echoing to server -> ")
+# while 1:    
+# 	count = count+1    
+# 	msgToSend = command_string    
+# 	s.sendall(msgToSend)    
+# 	data = s.recv(1024)    
+# 	print 'Received', repr(data)    
+# 	time.sleep(5)
 
 s.close()
--- server.py
+++ server.py
@@ -1,14 +1,38 @@
- # Echo server program
- import socket
- HOST = ''                # Symbolic name meaning all available interfaces
- PORT = 5000              # Arbitrary non-privileged port
- s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- s.bind((HOST, PORT))
- s.listen(1)
- conn, addr = s.accept()
- print 'Connected by', addr
- while 1:    
- 	data = conn.recv(1024)    
- 	if not data: break    
- 	conn.sendall(data)
- conn.close()
+# Echo server program
+# socket.sendto(string, address) for direct messages
+
+import socket, select, sys;
+
+ 
+PORT = 5000        # arbitrary non-privileged port
+HOST = 'localhost' # retreives host of machine this code is run on
+
+users = {}         # dictionary defined key = username, value = clientsocket
+onlineList = []    # list of online users
+
+# create tcp/ip socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(0)
+
+# bind our socket and listen for connections
+server.bind((HOST, PORT))
+server.listen(5) #queue of up to 5 connections
+
+#list of sockets I expect to read from
+inputs = [ server ] 
+
+#list of sockets I expect to write to
+outputs = [ ]
+
+
+while inputs:
+	readable, writable, exceptional = select.select(inputs, inputs, inputs)
+	connection, addr = s.accept()
+	print ("Connected by " + str(addr))
+	connection.send(bytes("Welcome to the server", "utf-8"))
+ # while 1:    
+ # 	data = connection.recv(1024)    
+ # 	if not data: break    
+ # 	connection.sendall(data)
+
+conn.close()
