fae0065e5a5ccce2beb421759eedf73118bc483f
nickmorris991 <nickmorris25@outlook.com>
2019-10-12 00:08:57 -0400

    added existing sockets data to server

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ HEADER_SIZE = 10   # used for variable message size
 PORT = 5000        # arbitrary non-privileged port
 HOST = "127.0.0.1" # retreives host of machine this code is run on
 
-users = {}         # dictionary key = username, value = clientsocket
+users = {}         # dictionary (key = username, value = clientsocket)
 onlineList = []    # list of online users
 
 # create tcp/ip socket
@@ -26,8 +26,10 @@ def retrieveData(cliConnection):
 		header = cliConnection.recv(HEADER_SIZE)
 		if not len(header):
 			return False
+
 		msgSize = int(header.decode('utf-8'))
 		data = cliConnection.recv(msgSize)
+
 		return {
 		'header': header,
 		'data': data
@@ -44,10 +46,27 @@ while inputs:
 	for socket in readable:
 		if socket is server:
 			# this branch handles connection requests
-			print("received a connect request from a client")
+			print("received a connection request from a client")
 			clientConnection, clientAddress = server.accept()
+			print ("successfully connection to" + str(clientConnection))
 
+			# retrieve client data
+			cliData = retrieveData(clientConnection)
 
+			# save users info
+			clients[clientConnection] = cliData
 
+			# append to running list of sockets
+			inputs.append(clientConnection) 
+			clientConnection.setBlocking(0)
+
+		else: 
+			# this branch handles existing sockets sending data
+			msg = retrieveData(socket)
+			print("existing socket sending data. Sent from: " + str(socket))
+
+			cliData = clients[socket]
+
+			print("message was: " + cliData['header'] + cliData['data'] + msg['header'] + msg['data'])
 
 conn.close()
