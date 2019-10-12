2bebc8c0d800da35daa9facefb0448ee3a59cf1d
nickmorris991 <nickmorris25@outlook.com>
2019-10-12 18:42:13 -0400

    updates to server

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 import select, socket, errno;
 import time
 
-count = 0
-HEADER_SIZE = 10   # used for variable message size 
 PORT = 5000        # arbitrary non-privileged port
 HOST = "127.0.0.1" # retreives host of machine this code is run on
 
@@ -10,12 +8,11 @@ HOST = "127.0.0.1" # retreives host of machine this code is run on
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-inputs = [client]
+while True:
+	command_string = input("ENTER COMMAND > ")
 
-while 1:
-	command_string = input("enter command > ")
-	count = count + 1
 	msgToSend = command_string
 	client.sendall(msgToSend.encode('utf-8'))
+
 	data = client.recv(1024)
-	print ("received " + str(data))
+	print(data.decode('utf-8'))
--- server.py
+++ server.py
@@ -1,47 +1,135 @@
-# Echo server program
-# socket.sendto(string, address) for direct messages
-
 import socket, select, sys;
 
-HEADER_SIZE = 10   # used for variable message size 
-PORT = 5000        # arbitrary non-privileged port
-HOST = "127.0.0.1" # retreives host of machine this code is run on
+PORT = 5000            # arbitrary non-privileged port
+HOST = "127.0.0.1"     # retreives host of machine this code is run on
 
-user_name = []     # list of registered user_names retreived from the stored file
-onlineList = []    # list of online users (defined as users who have logged in)
-inputs = []        # list of sockets
-connections = 0    # running number of active connections
+usernames = []         # list of registered usernames retreived from the stored file
+onlineList = []        # list of online users (defined as users who have logged in)
+inputs = []            # list of sockets
+connectedSockets = {}  # dict to save user info (key=cliSocket, value=IP)
 
-# create tcp/ip socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setblocking(0)
+acceptedCommands = ["register", "login", "logout", "sendmsg", "listusers"]
 
-# bind our socket and listen for connections
-server.bind((HOST, PORT))
-server.listen(5) #queue of up to 5 connections
+def getRegisteredUsers():
+	# if the file exists open it and read the names if not return empty list
+	usernamesList = []
 
-#list of sockets I expect to read from
-inputs.append(server)
+	try:
+		f = open("regUsernames.txt", "r")
+		names = f.read()
+		names = names.split()
+		# loop over names and add them to the list
+		i = 0
+		while (i < len(names)):
+			usernamesList.append(names[i])
+			i+=1
+		return usernamesList
+	except:
+		return usernamesList
 
-while inputs:
-	readable, writable, exceptional = select.select(inputs, inputs, inputs)
+def getData(cliSocket):
+	try:
+		message = cliSocket.recv(1024)
 
-	for socket in readable:
-		if socket is server:
-			# this branch handles connection requests from new clients
-			print("received a connection request from a client.")
-			clientConnection, clientAddress = server.accept()
-			print ("successful connection to: " + str(clientAddress))
+		if not len(message):
+			return False
 
-			# account for active connection
-			connection += 1
+		return message
+	except:
+		return False
 
-			# append to running list of sockets
-			clientConnection.setblocking(0)
-			inputs.append(clientConnection) 
-		else: 
-			# this branch handles existing sockets sending data (data from clients)
-			data = socket.recv(1024)
-			print("existing socket sending data. Sent from: " + str(socket))
-			print("existing socket sent data, the data was: " + str(data))
-			socket.send(bytes("recieved data", 'utf-8'))
+def checkArguments(commandName, messageArray, client):
+
+	if (commandName is "register"):
+		if (len(messageArray) != 3):
+			s.send(bytes("ERROR 201: Incorrect number of arguments provided", "utf-8"))
+		elif (messageArray[0].decode('utf-8') in usernames):
+			pass
+	elif (commandName is "login"):
+		pass
+	elif (commandName is "logout"):
+		pass
+	elif (commandName is "sendmsg"):
+		pass
+	elif (commandName is "listusers"):
+		pass
+
+def main():
+	# running number of active connections
+	connections = 0    
+
+	# create tcp/ip socket
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.setblocking(0)
+
+	# bind our socket and listen for connections
+	server.bind((HOST, PORT))
+	server.listen(5) #queue of up to 5 connections
+
+	# list of sockets I expect to read from
+	inputs.append(server)
+
+	# get registered users from saved file
+	usernames = getRegisteredUsers()
+
+	#manage input (clients) while server is running
+	while inputs:
+		readable, writable, exceptional = select.select(inputs, inputs, inputs)
+
+		for s in readable:
+			if s is server:
+				# this branch handles connection requests from new clients
+				print("received a connection request from a client.")
+				clientConnection, clientAddress = server.accept()
+				print ("successful connection to: " + str(clientAddress))
+				print ("client socket is: " + str(clientConnection))
+
+				# account for active connection
+				connections += 1
+
+				# save account info
+				connectedSockets[clientConnection] = clientAddress
+
+				# append to running list of sockets
+				clientConnection.setblocking(0)
+				inputs.append(clientConnection) 
+			else: 
+				# this branch handles existing sockets sending data (sent from clients)
+				data = getData(s)
+
+				#handle forced shutdowns from the client
+				if data is False:
+					print("Closed connection from: " + str(connectedSockets[s]))
+					inputs.remove(s)
+					del connectedSockets[s]
+					continue
+
+				# get IP address of user so we know who sent this data
+				userIP = connectedSockets[s]
+
+				# get message broken into meaningful pieces (['command', 'arg1', 'arg2', 'word1', 'word2'])
+				messageArray = data.split()
+				command = messageArray[0].decode('utf-8')
+
+				if (command not in acceptedCommands):
+					#send array message saying you didn't send an accepted command
+					s.send(bytes("ERROR 200: Received unaccepted command. Try again","utf-8"))
+				else: 
+					if (command is "register"):
+						# expected format: "register <username> <password>"
+						checkArguments("register", messageArray, s)
+
+					elif (command is "login"):
+						checkArguments("login", messageArray, s)
+
+					elif (command is "logout"):
+						checkArguments("logout", messageArray, s)
+
+					elif (command is "sendmsg"):
+						checkArguments("sendmsg", messageArray, s)
+
+					elif (command is "listusers"):
+						checkArguments("listusers", messageArray, s)
+
+if __name__ == '__main__':
+	main()
